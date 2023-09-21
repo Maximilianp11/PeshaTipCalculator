5cd2aafc7b60b3d17b0406cb9d19dda2595d4bfb
BT_2N2_08 <Sample@bths.edu>
2023-09-21 13:25:37 -0400

    Working while code

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome tot the tip calculator!");
+        System.out.println("Welcome to the tip calculator!");
         System.out.println("How many people are in your group: ");
         int numPeople = scan.nextInt();
         scan.nextLine();
@@ -12,8 +12,9 @@ public class Main {
         double item = 0;
         double total = 0;
 
-            while (item !<= 0) {
+            while (item != -1) {
 
+            item = 0;
             System.out.println("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
             item += scan.nextDouble();
             total += item;
