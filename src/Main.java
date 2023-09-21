87f6cac2171e96acb04cf5fd9e072a8ce6f37f2c
BT_2N2_08 <Sample@bths.edu>
2023-09-21 13:18:41 -0400

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,22 @@ public class Main {
         System.out.println("Welcome tot the tip calculator!");
         System.out.println("How many people are in your group: ");
         int numPeople = scan.nextInt();
+        scan.nextLine();
         System.out.println("What is the tip percentage? (0-100): ");
         int tipPercent = scan.nextInt();
-        double total
+        scan.nextLine();
+        double item = 0;
+        double total = 0;
 
-        while
+            while (item !<= 0) {
 
-        System.out.println("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
+            System.out.println("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
+            item += scan.nextDouble();
+            total += item;
+            scan.nextLine();
+            }
 
-
+        System.out.println("test");
+        System.out.println(total);
     }
 }
