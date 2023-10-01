1a02c915ed92f7a6f7c65f1d2600fcf895d9d48e
Max Pesha <max.pesha@gmail.com>
2023-10-01 19:27:58 -0400

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,40 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // this welcomes the user, asks how many people are in the group, and asks for the tip percentage
         System.out.println("Welcome to the tip calculator!");
         System.out.println("How many people are in your group: ");
         int numPeople = scan.nextInt();
         scan.nextLine();
         double tipPercent = 0;
-        if (numPeople >= 6) {
-            tipPercent = 25;
-        }
-        else {
-            System.out.println("What is the tip percentage? (0-100): ");
-            tipPercent = scan.nextInt();
-            scan.nextLine();
-        }
-        double item = 0;
+        System.out.println("What is the tip percentage? (0-100): ");
+        tipPercent = scan.nextInt();
+        scan.nextLine();
+        double item1 = 0;
+        double item2 = 0;
         double total = 0;
 
-            while (item != -1) {
+            // this is the loop where the user keeps inputting items until they type -1
+            while (item1 != -1) {
 
-            item = 0;
-            System.out.println("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
-            item += scan.nextDouble();
-            total += item;
+            item1 = 0;
+            System.out.println("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end) (-2 to remove previous item)");
+            item1 += scan.nextDouble();
+            total += item1;
+            if (item1 == -2) {
+                total += 2;
+                total -= item2;
+            }
+            item2 = 0;
+            item2 = item1;
+
             scan.nextLine();
+
             }
 
-        total += 1;
+        // this prints out all the information like cost per person, tip percentage, etc
+        total += 1; // this adds 1 to the total because -1 is added when stopping the loop
         System.out.println("----------------------------------");
         System.out.println("Total bill before tip: $" + String.format("%.2f" , total));
         System.out.println("Tip percentage: " + (int) tipPercent + "%");
@@ -41,6 +49,6 @@ public class Main {
         System.out.println("Tip per person: $" + String.format("%.2f" , perTip));
         double perTotal = (totalTip + total) / numPeople;
         System.out.println("Total cost per person: $" + String.format("%.2f" , perTotal));
-
+        System.out.println("----------------------------------");
     }
 }
